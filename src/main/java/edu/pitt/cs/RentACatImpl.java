9cda02ea6b5534e9e24635f88b2a472af0560957
Stephen Gwon <gwonstephen@gmail.com>
2024-11-21 17:33:50 -0500

    Update RentACatImpl.java

--- src/main/java/edu/pitt/cs/RentACatImpl.java
+++ src/main/java/edu/pitt/cs/RentACatImpl.java
@@ -19,6 +19,17 @@ public class RentACatImpl implements RentACat {
 
 	public boolean returnCat(int id) {
 		// TODO: Fill in
+		Cat cat = getCat(id);
+		if (cat != null) {
+			if (cat.getRented()) {
+				cat.returnCat();
+				System.out.println("Welcome back, " + cat.getName() + "!");
+				return true;
+			} else {
+				System.out.println(cat.getName() + " is already here!"); // Adjusted message
+				return false;
+			}
+		}
 		return false;
 	}
 
@@ -33,7 +44,14 @@ public class RentACatImpl implements RentACat {
 	 */
 
 	public boolean rentCat(int id) {
-		// TODO: Fill in
+		Cat cat = getCat(id);
+        if (cat != null && !cat.getRented()) {
+            cat.rentCat();
+            System.out.println(cat.getName() + " has been rented.");
+            return true;
+        }
+        System.out.println("Sorry, " + (cat != null ? cat.getName() : "Invalid cat ID") + " is not here!");
+
 		return false;
 	}
 
@@ -48,6 +66,12 @@ public class RentACatImpl implements RentACat {
 
 	public boolean renameCat(int id, String name) {
 		// TODO: Fill in
+		Cat cat = getCat(id);
+        if (cat != null) {
+            cat.renameCat(name);
+            return true;
+        }
+
 		return false;
 	}
 
@@ -63,7 +87,13 @@ public class RentACatImpl implements RentACat {
 
 	public String listCats() {
 		// TODO: Fill in
-		return "WRITE CODE FOR THIS";
+		StringBuilder sb = new StringBuilder();
+        for (Cat cat : cats) {
+            if (!cat.getRented()) {
+                sb.append(cat.toString()).append("\n");
+            }
+        }
+        return sb.toString();
 	}
 
 	/**
